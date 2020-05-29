0921f5c6ca6498bdefac07a9bdd095558f639a72
Rousan Ali <rousanali786@gmail.com>
2020-05-29 17:27:59 +0530

    Add 64-bit support

--- scripts/build.prod.sh
+++ scripts/build.prod.sh
@@ -5,13 +5,16 @@ JNI_LIBS_DIR=./app/libs
 cross build --target aarch64-linux-android --release
 cross build --target armv7-linux-androideabi --release
 cross build --target i686-linux-android --release
+cross build --target x86_64-linux-android --release
 
 rm -rf $JNI_LIBS_DIR
 mkdir $JNI_LIBS_DIR
 mkdir $JNI_LIBS_DIR/arm64-v8a
 mkdir $JNI_LIBS_DIR/armeabi-v7a
 mkdir $JNI_LIBS_DIR/x86
+mkdir $JNI_LIBS_DIR/x86_64
 
 cp target/aarch64-linux-android/release/libglobalcounter.so $JNI_LIBS_DIR/arm64-v8a/libglobalcounter.so
 cp target/armv7-linux-androideabi/release/libglobalcounter.so $JNI_LIBS_DIR/armeabi-v7a/libglobalcounter.so
 cp target/i686-linux-android/release/libglobalcounter.so $JNI_LIBS_DIR/x86/libglobalcounter.so
+cp target/x86_64-linux-android/release/libglobalcounter.so $JNI_LIBS_DIR/x86_64/libglobalcounter.so
